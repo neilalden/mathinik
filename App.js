1691d5cd39400f4bdc8074c7198c5952b7b6bcd4
neilescobin <escobin.neil@gmail.com>
2023-04-17 08:19:34 +0800

    .

--- App.js
+++ App.js
@@ -3,9 +3,42 @@ import Navigation from './src/Navigation';
 import {Provider} from 'react-redux';
 import {PersistGate} from 'redux-persist/integration/react';
 import stores from './src/services/redux/store';
+import {PermissionsAndroid} from 'react-native';
 
 const App = () => {
   const {store, persistor} = stores();
+  React.useEffect(() => {
+    (async () => {
+      try {
+        const granted = await PermissionsAndroid.request(
+          PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
+          {
+            title: 'Mathinik Storage Permission',
+            message:
+              'Mathinik needs access to your storage ' +
+              'so you can upload files from your storage',
+            buttonNeutral: 'Ask Me Later',
+            buttonNegative: 'Cancel',
+            buttonPositive: 'OK',
+          },
+        );
+      } catch (error) {}
+      try {
+        const granted = await PermissionsAndroid.request(
+          PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
+          {
+            title: 'Mathinik Storage Permission',
+            message:
+              'Mathinik needs access to your storage ' +
+              'so you can save files to your storage',
+            buttonNeutral: 'Ask Me Later',
+            buttonNegative: 'Cancel',
+            buttonPositive: 'OK',
+          },
+        );
+      } catch (error) {}
+    })();
+  }, []);
   return (
     <Provider store={store}>
       <PersistGate persistor={persistor}>
